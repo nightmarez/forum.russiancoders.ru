f39308961f14c5df42a8afd8337ee871e23accc7
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 13:55:34 +0300

    logout form

---
+++ logout.php
@@ -0,0 +1,30 @@
+<?php include_once('head.php'); ?>
+<?php include_once('nav.php'); ?>
+
+<div class="panel panel-primary" style="margin: 20px;">
+	<div class="panel-heading">
+		<h3 class="panel-title">Выход</h3>
+	</div>
+
+	<div class="panel-body">
+		<form action="/dologout.php" method="POST">
+			<div class="col-md-12 col-sm-12">
+				<div class="form-group col-md-12 col-sm-12">
+					<input type="submit" id="submit-button" class="btn btn-primary" value="Выйти в этом браузере">
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<div class="panel-body">
+		<form action="/dofulllogout.php" method="POST">
+			<div class="col-md-12 col-sm-12">
+				<div class="form-group col-md-12 col-sm-12">
+					<input type="submit" id="submit-button" class="btn btn-primary" value="Выйти на всех устройствах">
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+<?php include_once('footer.php'); ?>
