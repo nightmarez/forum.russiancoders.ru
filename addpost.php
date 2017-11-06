b1e327a4213bf9b734330b8de29656dd2fb82d5f
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-07 02:08:39 +0300

    add post code

---
+++ addpost.php
@@ -0,0 +1,21 @@
+<?php
+	require_once('utils.php');
+
+	if (!isLogin()) {
+		header('Location: /login.php');
+		die();
+	}
+
+	$content = $_POST['content'];
+
+	if ($content !== $_POST['content'] || strlen($content) > 4096) {
+		//header('Location: /createtopic.php?error=Некорректно задано содержимое поста');
+		die();
+	}
+
+	$userid = htmlspecialchars($_COOKIE['userid']);
+	$content = htmlspecialchars($_POST['topicid']);
+
+	addPost($userid, $topicid, $content);
+	header('Location: /topic.php?topicid=' . $topicid);
+?>
