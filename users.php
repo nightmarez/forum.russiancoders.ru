604698ecf63fe8b33f03559b3e1dae62c4fc4573
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 14:10:06 +0300

    users form

---
+++ users.php
@@ -0,0 +1,43 @@
+<?php include_once('head.php'); ?>
+<?php include_once('nav.php'); ?>
+
+<div class="panel panel-primary" style="margin: 20px;">
+	<div class="panel-heading">
+		<h3 class="panel-title">Регистрация</h3>
+	</div>
+
+	<div class="panel-body">
+		<div class="table-responsive">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Login</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$db = new PdoDb();
+
+						$query =
+							'SELECT `userid`, `login` FROM `users` ORDER BY `id` LIMIT 0, 1;';
+
+						$req = $db->prepare($query);
+						$req->execute();
+
+						while (list($userid, $login) = $req->fetch(PDO::FETCH_NUM)) {
+							?>
+								<tr>
+									<td>
+										<a href="/user.php?userid=<?php echo htmlspecialchars($userid); ?>"><?php echo htmlspecialchars($login); ?></a>
+									</td>
+								</tr>
+							<?php
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<?php include_once('footer.php'); ?>
