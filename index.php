67a5e69f1371a015704546c2b9258f5c00857bd1
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 19:04:56 +0300

    hotfix

--- index.php
+++ index.php
@@ -69,73 +69,77 @@
 
 		while (list($sectionid, $title) = $req->fetch(PDO::FETCH_NUM)) {
 	?>
+		<div class="panel panel-primary" style="margin: 20px;">
+			<div class="panel-heading">
+				<h3 class="panel-title"><a href="/section.php?sectionid=<?php echo htmlspecialchars($sectionid); ?>"><?php echo htmlspecialchars($title); ?></a></h3>
+			</div>
 
-		<div class="panel-body">
-			<div class="table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th><a href="/section.php?sectionid=<?php echo htmlspecialchars($sectionid); ?>"><?php echo htmlspecialchars($title); ?></a></th>
-							<th>Автор</th>
-							<th>Последний</th>
-							<th>Ответы</th>
-							<th>Обновление</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php
-							$pdo = new PdoDb();
+			<div class="panel-body">
+				<div class="table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Тема</th>
+								<th>Автор</th>
+								<th>Последний</th>
+								<th>Ответы</th>
+								<th>Обновление</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								$pdo = new PdoDb();
 
-							$query =
-								'SELECT `topicid`, `title`, `userid` FROM `topics` WHERE `sectionid`=:sectionid;';
+								$query =
+									'SELECT `topicid`, `title`, `userid` FROM `topics` WHERE `sectionid`=:sectionid;';
 
-							$r = $pdo->prepare($query);
-							$r->bindParam(':sectionid', $sectionid);
-							$r->execute();
-							
-							while (list($topicid, $title, $userid) = $r->fetch(PDO::FETCH_NUM)) {
-								?>
-									<tr>
-										<td>
-											<a href="/topic.php?topicid=<?php echo htmlspecialchars($topicid); ?>"><?php echo htmlspecialchars($title); ?></a>
-										</td>
-										<td>
-											<?php
-												$p = new PdoDb();
+								$r = $pdo->prepare($query);
+								$r->bindParam(':sectionid', $sectionid);
+								$r->execute();
+								
+								while (list($topicid, $title, $userid) = $r->fetch(PDO::FETCH_NUM)) {
+									?>
+										<tr>
+											<td style="width: 20%;">
+												<a href="/topic.php?topicid=<?php echo htmlspecialchars($topicid); ?>"><?php echo htmlspecialchars($title); ?></a>
+											</td>
+											<td style="width: 20%;">
+												<?php
+													$p = new PdoDb();
 
-												$query =
-													'SELECT `login` FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
+													$query =
+														'SELECT `login` FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
 
-												$rr = $p->prepare($query);
-												$rr->bindParam(':userid', $userid);
-												$rr->execute();
+													$rr = $p->prepare($query);
+													$rr->bindParam(':userid', $userid);
+													$rr->execute();
 
-												while (list($login) = $rr->fetch(PDO::FETCH_NUM)) {
-													?>
-														<a href="/user.php?userid=<?php echo htmlspecialchars($userid); ?>"><?php echo htmlspecialchars($login); ?></a>
-													<?php
-													break;
-												}
-											?>
-										</td>
-										<td>
-											?
-										</td>
-										<td>
-											?
-										</td>
-										<td>
-											?
-										</td>
-									</tr>
-								<?php
-							}
-						?>
-					</tbody>
-				</table>
+													while (list($login) = $rr->fetch(PDO::FETCH_NUM)) {
+														?>
+															<a href="/user.php?userid=<?php echo htmlspecialchars($userid); ?>"><?php echo htmlspecialchars($login); ?></a>
+														<?php
+														break;
+													}
+												?>
+											</td>
+											<td style="width: 20%;">
+												?
+											</td>
+											<td style="width: 20%;">
+												?
+											</td>
+											<td style="width: 20%;">
+												?
+											</td>
+										</tr>
+									<?php
+								}
+							?>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
-
 	<?php
 		}
 	?>
