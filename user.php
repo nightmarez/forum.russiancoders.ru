1ec8fdb291c6fae0f2c91621cbb4cd5c0a77fefb
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 17:50:59 +0300

    add user profile

---
+++ user.php
@@ -0,0 +1,68 @@
+<?php include_once('head.php'); ?>
+<?php include_once('nav.php'); ?>
+
+<?php
+	$userid = false;
+
+	if (isset($_GET['userid'])) {
+		$userid = htmlspecialchars($_GET['userid']);
+
+		if (!preg_match('/^\{?[0-9a-zA-Z]{20}\}?$/', $userid)) {
+			$userid = false;
+		}
+	}
+?>
+
+<div class="panel panel-primary" style="margin: 20px;">
+	<div class="panel-heading">
+		<h3 class="panel-title">Профиль пользователя</h3>
+	</div>
+
+	<div class="panel-body">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<?php if ($userid === false) { ?>
+						<tr>
+							<td>
+								<p>Пользователь с указанным идентификатором не найден</p>
+							</td>
+						</tr>
+					<?php } else { ?>
+						<?php
+							$db = new PdoDb();
+
+							$query =
+								'SELECT `login`, `last` FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
+
+							$req = $db->prepare($query);
+							$req->bindParam(':userid', $userid);
+							$req->execute();
+
+							while (list($userid, $last) = $req->fetch(PDO::FETCH_NUM)) {
+						?>
+							<tr>
+								<td colspan="2">
+									<h3><?php echo htmlspecialchars($login); ?><h3>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Последнее посещение:
+								</td>
+								<td>
+									<?php echo $last; ?>
+								</td>
+							</tr>
+						<?php
+								break;
+							}
+						?>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<?php include_once('footer.php'); ?>
