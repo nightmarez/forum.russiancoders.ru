a9f5fc6514f353244f64b19dd61cba4ba8e53c18
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 19:22:28 +0300

    hotfix

--- section.php
+++ section.php
@@ -41,71 +41,75 @@
 
 							while (list($title) = $req->fetch(PDO::FETCH_NUM)) {
 						?>
-								<div class="panel-body">
-									<div class="table-responsive">
-										<table class="table">
-											<thead>
-												<tr>
-													<th>Тема</th>
-													<th>Автор</th>
-													<th>Последний</th>
-													<th>Ответы</th>
-													<th>Обновление</th>
-												</tr>
-											</thead>
-											<tbody>
-												<?php
-													$pdo = new PdoDb();
+							<tr>
+								<td>
+									<div class="panel-body">
+										<div class="table-responsive">
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Тема</th>
+														<th>Автор</th>
+														<th>Последний</th>
+														<th>Ответы</th>
+														<th>Обновление</th>
+													</tr>
+												</thead>
+												<tbody>
+													<?php
+														$pdo = new PdoDb();
 
-													$query =
-														'SELECT `topicid`, `title`, `userid`, `updated` FROM `topics` WHERE `sectionid`=:sectionid ORDER BY `updated` DESC LIMIT 0, 20;';
+														$query =
+															'SELECT `topicid`, `title`, `userid`, `updated` FROM `topics` WHERE `sectionid`=:sectionid ORDER BY `updated` DESC LIMIT 0, 20;';
 
-													$r = $pdo->prepare($query);
-													$r->bindParam(':sectionid', $sectionid);
-													$r->execute();
-													
-													while (list($topicid, $title, $userid, $updated) = $r->fetch(PDO::FETCH_NUM)) {
-														?>
-															<tr>
-																<td style="width: 20%;">
-																	<a href="/topic.php?topicid=<?php echo htmlspecialchars($topicid); ?>"><?php echo htmlspecialchars($title); ?></a>
-																</td>
-																<td style="width: 20%;">
-																	<?php
-																		$p = new PdoDb();
+														$r = $pdo->prepare($query);
+														$r->bindParam(':sectionid', $sectionid);
+														$r->execute();
+														
+														while (list($topicid, $title, $userid, $updated) = $r->fetch(PDO::FETCH_NUM)) {
+															?>
+																<tr>
+																	<td style="width: 20%;">
+																		<a href="/topic.php?topicid=<?php echo htmlspecialchars($topicid); ?>"><?php echo htmlspecialchars($title); ?></a>
+																	</td>
+																	<td style="width: 20%;">
+																		<?php
+																			$p = new PdoDb();
 
-																		$query =
-																			'SELECT `login` FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
+																			$query =
+																				'SELECT `login` FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
 
-																		$rr = $p->prepare($query);
-																		$rr->bindParam(':userid', $userid);
-																		$rr->execute();
+																			$rr = $p->prepare($query);
+																			$rr->bindParam(':userid', $userid);
+																			$rr->execute();
 
-																		while (list($login) = $rr->fetch(PDO::FETCH_NUM)) {
-																			?>
-																				<a href="/user.php?userid=<?php echo htmlspecialchars($userid); ?>"><?php echo htmlspecialchars($login); ?></a>
-																			<?php
-																			break;
-																		}
-																	?>
-																</td>
-																<td style="width: 20%;">
-																	?
-																</td>
-																<td style="width: 20%;">
-																	?
-																</td>
-																<td style="width: 20%;">
-																	<?php echo $updated; ?>
-																</td>
-															</tr>
-														<?php
-													}
-												?>
-											</tbody>
-										</table>
+																			while (list($login) = $rr->fetch(PDO::FETCH_NUM)) {
+																				?>
+																					<a href="/user.php?userid=<?php echo htmlspecialchars($userid); ?>"><?php echo htmlspecialchars($login); ?></a>
+																				<?php
+																				break;
+																			}
+																		?>
+																	</td>
+																	<td style="width: 20%;">
+																		?
+																	</td>
+																	<td style="width: 20%;">
+																		?
+																	</td>
+																	<td style="width: 20%;">
+																		<?php echo $updated; ?>
+																	</td>
+																</tr>
+															<?php
+														}
+													?>
+												</tbody>
+											</table>
+										</div>
 									</div>
-								</div>
+								</td>
+							</tr>
 						<?php
 							}
 						?>
