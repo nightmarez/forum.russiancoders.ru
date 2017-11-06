0964a278f462b5559fc250c7b6b26f10e772dbf6
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-06 19:49:32 +0300

    faq

---
+++ faq.php
@@ -0,0 +1,56 @@
+<?php include_once('head.php'); ?>
+<?php include_once('nav.php'); ?>
+
+<div class="panel panel-primary" style="margin: 20px;">
+	<div class="panel-heading">
+		<h3 class="panel-title">ЧаВо</h3>
+	</div>
+
+	<div>
+		<ul class="list-group">
+			<li class="list-group-item">
+				<p class="big-letter" style="color: red;">Q:</p> Для чего разрабатывается этот форум?
+			</li>
+			<li class="list-group-item">
+				<p class="big-letter" style="color: blue;">A:</p> Для свободного общения людей, в первую очередь, айтишников, на различные тематики без
+				идиотских ограничений и модераторского беспредела, существующего на многих других форумах.
+			</li>
+		</ul>
+	</div>
+
+	<div>
+		<ul class="list-group">
+			<li class="list-group-item">
+				<p class="big-letter" style="color: red;">Q:</p> Что можно обсуждать на форуме?
+			</li>
+			<li class="list-group-item">
+				<p class="big-letter" style="color: blue;">A:</p> Всё, что душе угодно. Если для чего-то не найдётся раздела - не проблема его создать.
+			</li>
+		</ul>
+	</div>
+
+	<div>
+		<ul class="list-group">
+			<li class="list-group-item">
+				<p class="big-letter" style="color: red;">Q:</p> Чего нельзя делать на форуме?
+			</li>
+			<li class="list-group-item">
+				<p class="big-letter" style="color: blue;">A:</p> Заниматься откровенным вредительством, флудом и тупостью, выкладывать незаконные
+				материалы в попытках скомпрометировать форум.
+			</li>
+		</ul>
+	</div>
+
+	<div>
+		<ul class="list-group">
+			<li class="list-group-item">
+				<p class="big-letter" style="color: red;">Q:</p> Мне нахамили, показали жестокий контент, похлестали чреслами по устам!
+			</li>
+			<li class="list-group-item">
+				<p class="big-letter" style="color: blue;">A:</p> Идите в другое место, здесь не институт благородных девиц.
+			</li>
+		</ul>
+	</div>
+</div>
+
+<?php include_once('footer.php'); ?>
