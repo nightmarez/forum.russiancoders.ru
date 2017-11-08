9b4551045714db522c303e2f7b18b4b0b0ebf38c
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-08 18:44:50 +0300

    profile update

--- profile.php
+++ profile.php
@@ -59,11 +59,19 @@
 							</tr>
 							<tr>
 								<td colspan="2">
-									<form action="upload.php" method="post" enctype="multipart/form-data">
-									    Загрузка изображения:
-									    <input type="file" name="image" id="image">
-									    <input type="submit" value="Загрузить" name="submit">
-									</form>
+									<div class="input-group image-preview">
+										<input type="text" class="form-control image-preview-filename" disabled="disabled">
+										<span class="input-group-btn">
+											<button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+												<span class="glyphicon glyphicon-remove"></span> Очистить
+											</button>
+											<div class="btn btn-default image-preview-input">
+												<span class="glyphicon glyphicon-folder-open"></span>
+												<span class="image-preview-input-title">Выбрать</span>
+												<input type="file" accept="image/png, image/jpeg, image/gif" name="input-file-preview"/>
+											</div>
+										</span>
+									</div>
 								</td>
 							</tr>
 						<?php
