06f050bda51168dad45fe611dfc31a450cd11c11
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-09 19:40:40 +0300

    dont register exists login

--- doregister.php
+++ doregister.php
@@ -2,13 +2,29 @@
 	require_once('utils.php');
 	require_once('recaptchalib.php');
 
-	$login = $_POST['login'];
+	$login = trim($_POST['login']);
 	$pass1 = $_POST['pass1'];
 	$pass2 = $_POST['pass2'];
 	$mail = $_POST['mail'];
 
 	if ($pass1 !== $pass2) {
 		header('Location: /register.php?error=Пароли не совпадают');
+		die();
+	}
+
+	$db = new PdoDb();
+
+	$query =
+		'SELECT COUNT(*) FROM `users` WHERE `login`=:login LIMIT 0, 1;';
+
+	$req = $db->prepare($query);
+	$req->bindParam(':login', $login);
+	$req->bindParam(':userid', $userid);
+	$req->execute();
+	$count = $req->fetchColumn();
+	
+	if ($count > 0) {
+		header('Location: /register.php?error=Такой логин уже занят');
 		die();
 	}
 
