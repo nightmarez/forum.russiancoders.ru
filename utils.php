1a3ae513e0e30b6d91a5864620941fdad1b22b89
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-10 01:36:37 +0300

    fix filterMessage

--- utils.php
+++ utils.php
@@ -412,6 +412,8 @@
   		$text = preg_replace('/(\[br\]){2,}/i', '<br/><br/>', $text);
   		$text = preg_replace('/\[br\]/i', '<br/>', $text);
 
+  		$text = preg_replace('/^\s+>\s+(.+)<br/>/i', '<p style="color: darkgray;">${1}</p><br/>', $text);
+
   		$text = preg_replace('/\[img=([0-9a-zA-Z]{20})\]/i', '<img src="https://storage.russiancoders.ru/' . $userid . '/${1}.jpg">', $text);
 
   		return $text;
