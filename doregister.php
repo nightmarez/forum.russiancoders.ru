32c9310a486fc4174ec364ca6d59006ad256cc88
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-09 21:43:25 +0300

    update login registration processing

--- doregister.php
+++ doregister.php
@@ -2,26 +2,89 @@
 	require_once('utils.php');
 	require_once('recaptchalib.php');
 
-	$login = trim($_POST['login']);
-	$login = preg_replace('/а/', 'a', $login);
-	$login = preg_replace('/е/', 'e', $login);
-	$login = preg_replace('/о/', 'o', $login);
-	$login = preg_replace('/р/', 'p', $login);
-	$login = preg_replace('/с/', 'c', $login);
-	$login = preg_replace('/у/', 'y', $login);
-	$login = preg_replace('/х/', 'x', $login);
+	if (!isset($_POST['login'])) {
+		header('Location: /register.php?error=Не задан логин');
+		die();
+	}
 
-	$login = preg_replace('/А/', 'A', $login);
-	$login = preg_replace('/В/', 'B', $login);
-	$login = preg_replace('/Е/', 'E', $login);
-	$login = preg_replace('/К/', 'K', $login);
-	$login = preg_replace('/М/', 'M', $login);
-	$login = preg_replace('/Н/', 'H', $login);
-	$login = preg_replace('/О/', 'O', $login);
-	$login = preg_replace('/Р/', 'P', $login);
-	$login = preg_replace('/С/', 'C', $login);
-	$login = preg_replace('/Т/', 'T', $login);
-	$login = preg_replace('/Х/', 'X', $login);
+	$login = trim();
+
+	if (strlen($login) <= 0) {
+		header('Location: /register.php?error=Задан пустой логин');
+		die();
+	}
+
+	$firstSymbol = mb_substr($login, 0, 1, 'utf-8');
+
+	if (in_array($firstSymbol, /* latin symbols */ [
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9']))
+	{
+		$login = preg_replace('/а/', 'a', $login);
+		$login = preg_replace('/е/', 'e', $login);
+		$login = preg_replace('/о/', 'o', $login);
+		$login = preg_replace('/р/', 'p', $login);
+		$login = preg_replace('/с/', 'c', $login);
+		$login = preg_replace('/у/', 'y', $login);
+		$login = preg_replace('/х/', 'x', $login);
+
+		$login = preg_replace('/А/', 'A', $login);
+		$login = preg_replace('/В/', 'B', $login);
+		$login = preg_replace('/Е/', 'E', $login);
+		$login = preg_replace('/К/', 'K', $login);
+		$login = preg_replace('/М/', 'M', $login);
+		$login = preg_replace('/Н/', 'H', $login);
+		$login = preg_replace('/О/', 'O', $login);
+		$login = preg_replace('/Р/', 'P', $login);
+		$login = preg_replace('/С/', 'C', $login);
+		$login = preg_replace('/Т/', 'T', $login);
+		$login = preg_replace('/Х/', 'X', $login);
+	} else {
+		$login = preg_replace('/a/', 'а', $login);
+		$login = preg_replace('/e/', 'е', $login);
+		$login = preg_replace('/o/', 'о', $login);
+		$login = preg_replace('/p/', 'р', $login);
+		$login = preg_replace('/c/', 'с', $login);
+		$login = preg_replace('/y/', 'у', $login);
+		$login = preg_replace('/x/', 'х', $login);
+
+		$login = preg_replace('/A/', 'А', $login);
+		$login = preg_replace('/B/', 'В', $login);
+		$login = preg_replace('/E/', 'Е', $login);
+		$login = preg_replace('/K/', 'К', $login);
+		$login = preg_replace('/M/', 'М', $login);
+		$login = preg_replace('/H/', 'Н', $login);
+		$login = preg_replace('/O/', 'О', $login);
+		$login = preg_replace('/P/', 'Р', $login);
+		$login = preg_replace('/C/', 'С', $login);
+		$login = preg_replace('/T/', 'Т', $login);
+		$login = preg_replace('/X/', 'Х', $login);
+	}
+
+	$tmp = $login;
+	$len = mb_strlen($tmp, 'utf-8');
+	$login = '';
+
+	for ($i = 0; $i < $len; ++$i) {
+		$symbol = mb_substr($login, $i, 1, 'utf-8');
+
+		if (in_array($symbol, [
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я',
+			'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+			'!', '@', '#', '$', '%', '-', '+', '=', '^', '*', '(', ')', '[', ']']))
+		{
+			$login .= $symbol;
+		}
+	}
+
+	if (strlen($login) <= 0) {
+		header('Location: /register.php?error=Задан пустой логин');
+		die();
+	}
 
 	$pass1 = $_POST['pass1'];
 	$pass2 = $_POST['pass2'];
@@ -54,7 +117,7 @@
 	$req->execute();
 
 	while (list($l) = $req->fetch(PDO::FETCH_NUM)) {
-		if (levenshtein($login, $l, 1, 1, 1) < 7) {
+		if (levenshtein($login, $l, 1, 1, 1) <= 3) {
 			header('Location: /register.php?error=Похожий логин уже занят');
 			die();
 		}
