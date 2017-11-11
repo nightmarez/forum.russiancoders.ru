48fe727e787e4d39aa3ed4e2ef84ab53d4b6061e
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-11 12:04:53 +0300

    hotfix

--- gallery.php
+++ gallery.php
@@ -94,6 +94,8 @@
 				$files = scanDirectory('/var/www/domains/storage.russiancoders.ru/');
 
 				foreach ($files as $key => $value) {
+					echo '/var/www/domains/storage.russiancoders.ru/gallery/' . basename($value) . '<br>';
+
 					if (!file_exists('/var/www/domains/storage.russiancoders.ru/gallery/' . basename($value)) {
 						createThumbnail(basename($value), '200', '200', dirname($value), '/var/www/domains/storage.russiancoders.ru/gallery/');
 					}
