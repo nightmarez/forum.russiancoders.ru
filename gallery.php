1e931f9e3107f5dea2f319d8efa8f4d0292e9d6f
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-11 11:42:10 +0300

    gallery update

--- gallery.php
+++ gallery.php
@@ -29,7 +29,7 @@
 							} 
 							else
 							{ 
-								$result[] = $value; 
+								$result[] = $base . DIRECTORY_SEPARATOR . $value; 
 							} 
 						} 
    					} 
@@ -37,10 +37,65 @@
    					return $result; 
 				}
 
+				function createThumbnail($image_name, $new_width, $new_height, $uploadDir, $moveToDir)
+				{
+					$path = $uploadDir . '/' . $image_name;
+					$mime = getimagesize($path);
+
+					if ($mime['mime'] == 'image/png') { 
+						$src_img = imagecreatefrompng($path);
+					}
+
+					if ($mime['mime']=='image/jpg' || $mime['mime']=='image/jpeg' || $mime['mime']=='image/pjpeg') {
+						$src_img = imagecreatefromjpeg($path);
+					}   
+
+					$old_x = imageSX($src_img);
+					$old_y = imageSY($src_img);
+
+					if ($old_x > $old_y) 
+					{
+						$thumb_w = $new_width;
+						$thumb_h = $old_y * ($new_height / $old_x);
+					}
+
+					if ($old_x < $old_y) 
+					{
+						$thumb_w = $old_x*($new_width/$old_y);
+						$thumb_h = $new_height;
+					}
+
+					if ($old_x == $old_y) 
+					{
+						$thumb_w = $new_width;
+						$thumb_h = $new_height;
+					}
+
+					$dst_img = ImageCreateTrueColor($thumb_w,$thumb_h);
+					imagecopyresampled($dst_img,$src_img,0,0,0,0,$thumb_w,$thumb_h,$old_x,$old_y); 
+					$new_thumb_loc = $moveToDir . $image_name;
+
+					if ($mime['mime'] == 'image/png') {
+						$result = imagepng($dst_img,$new_thumb_loc,8);
+					}
+
+					if($mime['mime'] == 'image/jpg' || $mime['mime'] == 'image/jpeg' || $mime['mime'] == 'image/pjpeg') {
+						$result = imagejpeg($dst_img,$new_thumb_loc,80);
+					}
+
+					imagedestroy($dst_img); 
+					imagedestroy($src_img);
+
+					return $result;
+				}
+
 				$files = scanDirectory('/var/www/domains/storage.russiancoders.ru/');
 
 				foreach ($files as $key => $value) {
 					echo $value . '<br>';
+					//if (!file_exists('/var/www/domains/storage.russiancoders.ru/gallery/' . $value)) {
+					//	createThumbnail($value, '200', '200', )
+					//}
 				}
 			?>
 
