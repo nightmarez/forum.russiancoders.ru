9f8590846633da5eb6042086c43ec8bfd45ec210
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-11 20:36:38 +0300

    update online mechanism

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
+
 $(document).ready(function() {
 	var ping = function() {
 		setTimeout(function() {
-			$.get('https://forum.russiancoders.ru/ping.php');
+			$.get('https://forum.russiancoders.ru/ping.php?uniq=' + uuidv4());
+			ping();
 		}, 60000);
 	};
 
