b9d62183a8eabd06aa292d3d220add7c554a86d1
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-13 13:25:11 +0300

    update uploader

--- main.js
+++ main.js
@@ -32,3 +32,41 @@ $(document).ready(function() {
 		$('html, body').animate({ scrollTop: $(document).height() }, 'fast');
 	}
 });
+
+$(document).ready(function() {
+	if (location.href.indexOf('/uploader/') !== -1) {
+		$(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+		});
+
+		$('.btn-file :file').on('fileselect', function(event, label) {
+			
+			var input = $(this).parents('.input-group').find(':text'),
+				log = label;
+			
+			if( input.length ) {
+				input.val(log);
+			} else {
+				if( log ) alert(log);
+			}
+		
+		});
+		function readURL(input) {
+			if (input.files && input.files[0]) {
+				var reader = new FileReader();
+				
+				reader.onload = function (e) {
+					$('#img-upload').attr('src', e.target.result);
+				};
+				
+				reader.readAsDataURL(input.files[0]);
+			}
+		}
+
+		$("#imgInp").change(function(){
+			readURL(this);
+		});
+	}
+});
--- uploader.php
+++ uploader.php
@@ -78,42 +78,4 @@
 	</div>
 </div>
 
-<script defer>
-	$(document).ready( function() {
-		$(document).on('change', '.btn-file :file', function() {
-		var input = $(this),
-			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-		input.trigger('fileselect', [label]);
-		});
-
-		$('.btn-file :file').on('fileselect', function(event, label) {
-			
-			var input = $(this).parents('.input-group').find(':text'),
-				log = label;
-			
-			if( input.length ) {
-				input.val(log);
-			} else {
-				if( log ) alert(log);
-			}
-		
-		});
-		function readURL(input) {
-			if (input.files && input.files[0]) {
-				var reader = new FileReader();
-				
-				reader.onload = function (e) {
-					$('#img-upload').attr('src', e.target.result);
-				};
-				
-				reader.readAsDataURL(input.files[0]);
-			}
-		}
-
-		$("#imgInp").change(function(){
-			readURL(this);
-		}); 	
-	});
-</script>
-
 <?php include_once('footer.php'); ?>
