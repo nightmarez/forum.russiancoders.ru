55b15855628874a5840565a5140d7ec7c36dcdf6
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-16 02:25:32 +0300

    posts likes update

--- topic.php
+++ topic.php
@@ -63,7 +63,7 @@
 													echo $created;
 												?></td>
 											<td>
-												<span class="triangle-up <?php if (!canVote($id, $userid, $db)) { echo 'triangle-disabled'; } ?>"></span><span class="likes-counter"><?php echo calcPostVotes($id, $db); ?></span><span class="triangle-down <?php if (!canVote($id, $userid, $db)) { echo 'triangle-disabled'; } ?>"></span>
+												<span class="triangle-up <?php if (!canVote($id, $userid, $db)) { echo 'triangle-up-disabled'; } ?>"></span><span class="likes-counter"><?php echo calcPostVotes($id, $db); ?></span><span class="triangle-down <?php if (!canVote($id, $userid, $db)) { echo 'triangle-down-disabled'; } ?>"></span>
 											</td>
 										</tr>
 										<tr>
