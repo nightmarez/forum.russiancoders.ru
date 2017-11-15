37bd036c9b74636af61a69c73f593e5855740d24
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-15 20:56:47 +0300

    hotfix

--- topic.php
+++ topic.php
@@ -32,7 +32,7 @@
 						$db = new PdoDb();
 
 						$query =
-							'SELECT `id`, `userid`, `content`, `created` FROM `posts` WHERE `topicid`=:topicid ORDER BY `id` ASC LIMIT 0, 100;';
+							'SELECT `id`, `userid`, `content`, `created` FROM `posts` WHERE `topicid`=:topicid ORDER BY `id` ASC LIMIT 0, 300;';
 
 						$req = $db->prepare($query);
 						$req->bindParam(':topicid', $topicid);
