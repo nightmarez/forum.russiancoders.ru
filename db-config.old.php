42e4d841cbde7a0fe3c9c90e533333b5cb74f0e5
root <root@ovz1.nightmarez.z8qgz.vps.myjino.ru>
2017-11-16 06:13:18 -0500

    move db

---
+++ db-config.old.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_NAME', 'nightmarez_forum');
+	define('DB_USER', 'nightmarez');
+	define('DB_PASS', 'eyr374jrhtyfoe');
+	define('RE_SECRET', '6LdlUjcUAAAAAJXEqnJ9WpVHs4onHmzg93kBAPD2');
+	define('SMTP_PASS', 'F2@k?-\Lfz\3<P');
+?>
---
+++ db-config.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_NAME', 'nightmarez_forum');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'Righoo3oophip7d@');
+	define('RE_SECRET', '6LdlUjcUAAAAAJXEqnJ9WpVHs4onHmzg93kBAPD2');
+	define('SMTP_PASS', 'F2@k?-\Lfz\3<P');
+?>
