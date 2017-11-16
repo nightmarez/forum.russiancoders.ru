e04729b7960dd6ae081690c303923123cadf6245
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-16 04:07:20 +0300

    posts likes update

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
 
 				(function(self) {
 					$.get('/vote.php?id=' + id + '&userid=' + userid, function(result) {
-						result = JSON.parse(result.trim());
+						//result = JSON.parse(result.trim());
 
 						if (result.answer === true) {
 							self.addClass('triangle-up-disabled');
