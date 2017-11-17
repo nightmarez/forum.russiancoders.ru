70498e46f9990cc4ca5c9dcd567b6c8a6e93923b
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-18 00:34:25 +0300

    avatars in online users list

--- online.php
+++ online.php
@@ -23,16 +23,17 @@
 						$db = new PdoDb();
 
 						$query =
-							'SELECT `userid`, `login`, `last`, (now() - `last`) as `online` FROM `users` WHERE TIME_TO_SEC(TIMEDIFF(NOW(), `last`)) <= 24 * 60 * 60
+							'SELECT `userid`, `login`, `last`, (now() - `last`) as `online`, MD5(LOWER(TRIM(`mail`))) FROM `users` WHERE TIME_TO_SEC(TIMEDIFF(NOW(), `last`)) <= 24 * 60 * 60
  ORDER BY `last` DESC LIMIT 0, 100;';
 
 						$req = $db->prepare($query);
 						$req->execute();
 
-						while (list($userid, $login, $last, $online) = $req->fetch(PDO::FETCH_NUM)) {
+						while (list($userid, $login, $last, $online, $mail) = $req->fetch(PDO::FETCH_NUM)) {
 							?>
 								<tr>
 									<td>
+										<img style="margin-right: 15px;" src="<?php echo 'https://secure.gravatar.com/avatar/' . $mail . '.jpg?s=200';?>" align="left">
 										<a href="/user/<?php echo htmlspecialchars($userid); ?>/" style="float: left;"><?php echo htmlspecialchars($login); ?></a><?php if (intval($online <= 80 /* seconds */)) { ?><div class="online-indicator"></div><?php } ?>
 									</td>
 									<td>
