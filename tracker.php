f4a1876e8ff498bab55869dac7cd5d8d5c1b1e57
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-24 05:48:52 +0300

    hotfix

--- tracker.php
+++ tracker.php
@@ -24,47 +24,51 @@
 				?>
 					<div class="panel panel-info" style="margin: 20px;">
 						<div class="panel-heading">
-							<div class="col-md-1">
-								<?php
-									$query = 'SELECT MD5(LOWER(TRIM(`mail`))) FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
+							<div class="row">
+								<div class="col-md-1">
+									<?php
+										$query = 'SELECT MD5(LOWER(TRIM(`mail`))) FROM `users` WHERE `userid`=:userid LIMIT 0, 1;';
 
-									$r = $readydb->prepare($query);
-									$r->bindParam(':userid', $userid);
-									$r->execute();
+										$r = $readydb->prepare($query);
+										$r->bindParam(':userid', $userid);
+										$r->execute();
 
-									while (list($mail) = $r->fetch(PDO::FETCH_NUM)) {
-								?>
-									<img style="margin-right: 5px;" src="<?php echo 'https://secure.gravatar.com/avatar/' . $mail . '.jpg?s=25';?>" alt="<?php echo $login; ?>">
-								<?php
-									break;
-									}
-								?>
+										while (list($mail) = $r->fetch(PDO::FETCH_NUM)) {
+									?>
+										<img style="margin-right: 5px;" src="<?php echo 'https://secure.gravatar.com/avatar/' . $mail . '.jpg?s=25';?>" alt="<?php echo $login; ?>">
+									<?php
+										break;
+										}
+									?>
+								</div>
+								<div class="col-md-6">
+									<?php
+										$topicTitle = getTopicTitleById($topicid, $readydb);
+										$sectionid = getSectionIdByTopicId($topicid, $readydb);
+										$sectionTitle = getSectionTitleById($sectionid, $readydb);
+										$postnumber = getPostNumber($topicid, $id, $readydb);
+										$page = getPostPageNumber($topicid, $id, $readydb);
+									?>
+									<a href="/">Форум</a>
+									→
+									<a href="/section/<?php echo $sectionid; ?>/"><?php echo $sectionTitle; ?></a>
+									→
+									<a href="/topic/<?php echo $topicid; ?>/"><?php echo $topicTitle; ?></a>
+									→
+									<a href="/topic/<?php echo $topicid; ?>/<?php echo $page; ?>/">страница <?php echo $page; ?></a>
+									→
+									<a href="/topic/<?php echo $topicid; ?>/<?php echo $page; ?>/#<?php echo $postnumber; ?>">#<?php echo $postnumber; ?></a>
+								</div>
+								<div class="col-md-3">
+									<a href="/user/<?php echo htmlspecialchars($userid); ?>/"><?php echo $login; ?></a>
+								</div>
+								<div class="col-md-2"><?php echo $created; ?></div>
 							</div>
-							<div class="col-md-6">
-								<?php
-									$topicTitle = getTopicTitleById($topicid, $readydb);
-									$sectionid = getSectionIdByTopicId($topicid, $readydb);
-									$sectionTitle = getSectionTitleById($sectionid, $readydb);
-									$postnumber = getPostNumber($topicid, $id, $readydb);
-									$page = getPostPageNumber($topicid, $id, $readydb);
-								?>
-								<a href="/">Форум</a>
-								→
-								<a href="/section/<?php echo $sectionid; ?>/"><?php echo $sectionTitle; ?></a>
-								→
-								<a href="/topic/<?php echo $topicid; ?>/"><?php echo $topicTitle; ?></a>
-								→
-								<a href="/topic/<?php echo $topicid; ?>/<?php echo $page; ?>/">страница <?php echo $page; ?></a>
-								→
-								<a href="/topic/<?php echo $topicid; ?>/<?php echo $page; ?>/#<?php echo $postnumber; ?>">#<?php echo $postnumber; ?></a>
-							</div>
-							<div class="col-md-3">
-								<a href="/user/<?php echo htmlspecialchars($userid); ?>/"><?php echo $login; ?></a>
-							</div>
-							<div class="col-md-2"><?php echo $created; ?></div>
 						</div>
 						<div class="panel-body">
-							<?php echo filterMessage($content, $userid); ?>
+							<div class="row">
+								<?php echo filterMessage($content, $userid); ?>
+							</div>
 						</div>
 					</div>
 				<?php
