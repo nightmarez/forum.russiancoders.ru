db1ad39efcb2525b8d8cc219c3120a5e1b3ece31
Mikhail Makarov <m.m.makarov@gmail.com>
2017-11-25 04:30:43 +0300

    update page messages

--- messages.php
+++ messages.php
@@ -12,14 +12,13 @@
 		}
 
 		$userid = $_COOKIE['userid'];
-		$db = new PdoDb();
 
 		$query =
 			'SELECT `fromid`, `toid`, `text`, `last` 
 			FROM `messages`
 			WHERE `fromid`=:userid OR `toid`=:userid ORDER BY `id` ASC;';
 
-		$req = $db->prepare($query);
+		$req = $readydb->prepare($query);
 		$req->bindParam(':userid', $userid);
 		$req->execute();
 	?>
@@ -29,22 +28,27 @@
 			<?php
 				while (list($fromid, $toid, $text, $last) = $req->fetch(PDO::FETCH_NUM)) {
 			?>
-				<table class="table topic-posts">
-					<tbody>
-						<tr>
-							<td style="width: 200px;">From: <?php echo getUserLoginById($fromid); ?></td>
-							<td style="width: 200px;">To: <?php echo getUserLoginById($toid); ?></td>
-							<td><?php echo $last; ?></td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td colspan="4"><?php echo filterMessage($text, $userid); ?></td>
-						</tr>
-						<tr>
-							<td colspan="4">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-md-4">
+								От: <a href="/user/<?php echo htmlspecialchars($fromid); ?>/"><?php echo getUserLoginById($fromid, $readydb); ?></a>
+							</div>
+							<div class="col-md-4">
+								Кому: <a href="/user/<?php echo htmlspecialchars($toid); ?>/"><?php echo getUserLoginById($toid, $readydb); ?></a>
+							</div>
+							<div class="col-md-4" style="text-align: right;"><?php echo $last; ?></div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<?php echo filterMessage($text, $userid); ?>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
 								<input type="button" class="btn btn-primary" value="Ответить" data-id="<?php echo $fromid; ?>">
-							</td>
-						</tr>
+							</div>
+						</div>
 					</tbody>
 				</table>
 			<?php
