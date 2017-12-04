f2ccc5dafaa308e85ce87e1714ce1173b66ec775
Mikhail Makarov <m.m.makarov@gmail.com>
2017-12-04 14:33:26 +0300

    update yandex metrika"

--- footer.php
+++ footer.php
@@ -10,6 +10,12 @@
 				<p>Hosted at <a href="https://www.jino.ru/?pl=14468430">jino.ru</a></p>
 			</div>
 		</footer>
+<!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=46607505&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/46607505/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="46607505" data-lang="ru" /></a>
+<!-- /Yandex.Metrika informer -->
+
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
     (function (d, w, c) {
