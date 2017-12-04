e9f2d4bd2b8c334825307a4a56aeb513bceb869f
Mikhail Makarov <m.m.makarov@gmail.com>
2017-12-04 08:26:02 +0300

    update filterMessage

--- utils.php
+++ utils.php
@@ -1344,11 +1344,11 @@
 		$text = preg_replace('#\[sex\]#iUs', '<img src="https://gdpanel.nightmarez.net/sex.gif" alt="ёбля">', $text);
 
 		$text = preg_replace(
-			'#\[img=\"([0-9a-zA-Z]{20})\"\s*alt=\"([\S\s]{1,100})\"\]#iUs',
+			'#\[img=\"([0-9a-zA-Z]{20})\"\s*alt=\"([\S\s]{1,100})\"\]#iuUs',
 			'<img src="https://storage.russiancoders.ru/' . $userid . '/${1}.jpg" alt="${2}">',
 			$text);
 		$text = preg_replace(
-			'#\[img\s*alt=\"([\S\s]{1,100})\"\]([0-9a-zA-Z]{20})\s*\[\/img\]#iUs',
+			'#\[img\s*alt=\"([\S\s]{1,100})\"\]([0-9a-zA-Z]{20})\s*\[\/img\]#iuUs',
 			'<img src="https://storage.russiancoders.ru/' . $userid . '/${2}.jpg" alt="${1}">',
 			$text);
 		$text = preg_replace(
@@ -1361,11 +1361,11 @@
 			$text);
 
 		$text = preg_replace(
-			'#\[img=([0-9a-zA-Z]{20})\s*alt=([\S\s]{1,100})\]#iUs',
+			'#\[img=([0-9a-zA-Z]{20})\s*alt=([\S\s]{1,100})\]#iuUs',
 			'<img src="https://storage.russiancoders.ru/' . $userid . '/${1}.jpg" alt="${2}">',
 			$text);
 		$text = preg_replace(
-			'#\[img\s*alt=([\S\s]{1,100})\]([0-9a-zA-Z]{20})\s*\[\/img\]#iUs',
+			'#\[img\s*alt=([\S\s]{1,100})\]([0-9a-zA-Z]{20})\s*\[\/img\]#iuUs',
 			'<img src="https://storage.russiancoders.ru/' . $userid . '/${2}.jpg" alt="${1}">',
 			$text);
 		$text = preg_replace(
