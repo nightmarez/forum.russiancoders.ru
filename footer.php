f3d062d3fea4937baa2833428c434860dc97b4fa
Mikhail Makarov <m.m.makarov@gmail.com>
2017-12-04 14:43:01 +0300

    update yandex metrika"

--- footer.php
+++ footer.php
@@ -6,11 +6,11 @@ style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" tit
 <!-- /Yandex.Metrika informer -->
 
 			<div class="footer-inner">
-                <?php
-                    $end_time = microtime(true);
-                    $total_time = $end_time - $start_time;
-                    printf("<!-- Страница сгенерирована за %f секунд -->", $total_time);
-                ?>
+				<?php
+					$end_time = microtime(true);
+					$total_time = $end_time - $start_time;
+					printf("<!-- Страница сгенерирована за %f секунд -->", $total_time);
+				?>
 				<p>Developed by Michael Makarov (aka NightmareZ)</p>
 				<p>Welcome to my personal site <a href="https://nightmarez.net/">NightmareZ.net</a></p>
 				<p>Hosted at <a href="https://www.jino.ru/?pl=14468430">jino.ru</a></p>
@@ -18,29 +18,29 @@ style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" tit
 		</footer>
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter46607505 = new Ya.Metrika({
-                    id:46607505,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            } catch(e) { }
-        });
+	(function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+			try {
+				w.yaCounter46607505 = new Ya.Metrika({
+					id:46607505,
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true
+				});
+			} catch(e) { }
+		});
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+		var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = "https://mc.yandex.ru/metrika/watch.js";
 
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
+		if (w.opera == "[object Opera]") {
+			d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+	})(document, window, "yandex_metrika_callbacks");
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/46607505" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
