9fe46bf0a3a1770f9a01a1833d0c08105c04a028
NightmareZ <m.m.makarov@gmail.com>
2017-12-08 10:42:58 +0300

    hotfix

--- utils.php
+++ utils.php
@@ -150,13 +150,13 @@
 	}
 
 	isUserBanned($userid, $readydb = NULL) {
-		if (!validateUserId($userId)) {
+		if (!validateUserId($userid)) {
 			return false;
 		}
 
 		$db = is_null($readydb) ? new PdoDb() : $readydb;
 		$query = 'SELECT * FROM `users` WHERE `userid`=:userid AND `state`=0 LIMIT 0, 1;';
-		$req->bindParam(':userid', $userId);
+		$req->bindParam(':userid', $userid);
 		$req->execute();
 		$count = $req->fetchColumn();
 		return $count >= 1;
