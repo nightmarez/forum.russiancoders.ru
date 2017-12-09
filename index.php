f5523b1248565c58ad6f7376b7de98f0740cd136
NightmareZ <m.m.makarov@gmail.com>
2017-12-09 11:55:43 +0300

    index styles fix

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 													if (isPinnedTopic($topicid, $readydb)) {
 														++$counter;
 												?>
-													<div style="width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QwDBxEzL8906wAAAR1JREFUOMtjYCASPPexYX3uY8NAFnjuYyP/3MfmHpSNIsdEpBn2DAwMis99bC5LbjnCcNHRiHgDnvvYuDIwMCxkYGBgYGRk1LnsZHxFf/85hgOW2gwMDAwMjERo3gXVzPD66zeGN99+MDAyMlxxOH5V94ClNm4D8GiGgQcMDAyqLGia1BkYGNYzMDD8ZmBg0MOjOYyBgeGww/Grfxix2JzLwMAwCY9mG4fjV4/COFi98MLDIpORiWnaqx8/8WrGGQs32mZznDKyY3j79RtezVgNOHT8dMmdG1f7jgiKpzExMGRDhc9g08yATfOcZSv+V3d0ZsDEDlhq5x6w1L5AhOYz+eiakQxRJmjA3GUr/1V3dKaSlVmmLVzMxjBQAAAKYnxfnVVxpAAAAABJRU5ErkJggg==');" title="Прикреплённая тема"></div>
+													<div style="margin-right: 5px; width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QwDBxEzL8906wAAAR1JREFUOMtjYCASPPexYX3uY8NAFnjuYyP/3MfmHpSNIsdEpBn2DAwMis99bC5LbjnCcNHRiHgDnvvYuDIwMCxkYGBgYGRk1LnsZHxFf/85hgOW2gwMDAwMjERo3gXVzPD66zeGN99+MDAyMlxxOH5V94ClNm4D8GiGgQcMDAyqLGia1BkYGNYzMDD8ZmBg0MOjOYyBgeGww/Grfxix2JzLwMAwCY9mG4fjV4/COFi98MLDIpORiWnaqx8/8WrGGQs32mZznDKyY3j79RtezVgNOHT8dMmdG1f7jgiKpzExMGRDhc9g08yATfOcZSv+V3d0ZsDEDlhq5x6w1L5AhOYz+eiakQxRJmjA3GUr/1V3dKaSlVmmLVzMxjBQAAAKYnxfnVVxpAAAAABJRU5ErkJggg==');" title="Прикреплённая тема"></div>
 												<?php
 													}
 												?>
@@ -64,7 +64,7 @@
 												<?php
 													if (isTopicClosed($topicid, $readydb)) {
 												?>
-													<div style="width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAQCAYAAAAmlE46AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAB3RJTUUH4QwCDQkOQJdCEAAAAcxJREFUKM+Vkc1LlFEUxn/n3juvzpSMUhQpFbaYkqB2UkTYShclYUTU0CroX2gTIURraV0RtOgDJCKo6GMVDUJtrLBEqKA29iEODU4zOu+9p8XrhKMvRb/V4fA89znnXGGZvv6DTL96wYEjx/PACVUdVFUxIiXnohvP798uswIBGDx5hqd3rjNw9FTP0lL9WQihT0RmFQKqPSLyybnMcOnB+Pum0QJ8nJoEoHtHYTyEsM9ad9E6d8FYexOYRrWoqvne3Xsff5l5FwO45guHjhV7a9XqfmPsm4lHd0eb/cPFs1Pz5bl+VR2JG41zQA3ANAW/6vF2RSKvTKzc5eGtqz5WU1JkXbVhM392rF3u6IxonJ6pdAy8/N41siVbez209duTJZ+IFJic69zztpwfGt42e2VDtPg5uLZrsjjWft6E+iUjYEyijAMtWAExqPeIAmoy95wQ7wIICsG3nluXa6+AT34AAPXdBvCsQgQaAXxI6hSCS+v+rMGHH8nIhc3QlQXVVo1ZbVKgUk8SBfhaAZOSuiZRgPVtENkkZWNubVqqESDfDjs3Jem5KDlcmlHSjpOL+CtGkXn+G6k4Nbkx1YUCaB4I/3KALASbHf0NdRSy7ZQ3MF8AAAAASUVORK5CYII=');" title="Закрытая тема"></div>
+													<div style="margin-right: 5px; width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAQCAYAAAAmlE46AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAB3RJTUUH4QwCDQkOQJdCEAAAAcxJREFUKM+Vkc1LlFEUxn/n3juvzpSMUhQpFbaYkqB2UkTYShclYUTU0CroX2gTIURraV0RtOgDJCKo6GMVDUJtrLBEqKA29iEODU4zOu+9p8XrhKMvRb/V4fA89znnXGGZvv6DTL96wYEjx/PACVUdVFUxIiXnohvP798uswIBGDx5hqd3rjNw9FTP0lL9WQihT0RmFQKqPSLyybnMcOnB+Pum0QJ8nJoEoHtHYTyEsM9ad9E6d8FYexOYRrWoqvne3Xsff5l5FwO45guHjhV7a9XqfmPsm4lHd0eb/cPFs1Pz5bl+VR2JG41zQA3ANAW/6vF2RSKvTKzc5eGtqz5WU1JkXbVhM392rF3u6IxonJ6pdAy8/N41siVbez209duTJZ+IFJic69zztpwfGt42e2VDtPg5uLZrsjjWft6E+iUjYEyijAMtWAExqPeIAmoy95wQ7wIICsG3nluXa6+AT34AAPXdBvCsQgQaAXxI6hSCS+v+rMGHH8nIhc3QlQXVVo1ZbVKgUk8SBfhaAZOSuiZRgPVtENkkZWNubVqqESDfDjs3Jem5KDlcmlHSjpOL+CtGkXn+G6k4Nbkx1YUCaB4I/3KALASbHf0NdRSy7ZQ3MF8AAAAASUVORK5CYII=');" title="Закрытая тема"></div>
 												<?php
 													}
 												?>
@@ -72,7 +72,7 @@
 												<?php
 													if (isHotTopic($topicid, $readydb)) {
 												?>
-													<div style="width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAANCAYAAAB/9ZQ7AAABA0lEQVR4AWNAB7c9Vdg/xDjvehbpIMJACLyPdgr8EOPy/32089VXoQ48OBW+DHdSfh/t8gykGKqhEbuJkY72H6KdX4MUwXG0y3WoNCNc4YcoFxegO3/BFMFNjnH5ApF3Knkbbc7HAHSXCtDqjyBJLIofvox0FgfKP30X7ZzFAHTXAogkJgYpAuJHUCftZAASb1EVOH/GphHkHwaQu5AU3n8f4+iH3RbnbyBn7EOyNgkUGUD2X0z3O19keB/uqA9kvAcJvItxtoVEjPNmDMVRLjXgoHsT4WgO8vmHaKdUaFAqgdyIZOq5q6HabPCwfuZrzPU20tkDHklRTvIgJwLxYli0AwDQPva9BmGXugAAAABJRU5ErkJggg==');" title="Горячая тема"></div>
+													<div style="margin-right: 5px; width: 16px; height: 16px; float: left; background-position: center center; background-repeat: no-repeat; background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAANCAYAAAB/9ZQ7AAABA0lEQVR4AWNAB7c9Vdg/xDjvehbpIMJACLyPdgr8EOPy/32089VXoQ48OBW+DHdSfh/t8gykGKqhEbuJkY72H6KdX4MUwXG0y3WoNCNc4YcoFxegO3/BFMFNjnH5ApF3Knkbbc7HAHSXCtDqjyBJLIofvox0FgfKP30X7ZzFAHTXAogkJgYpAuJHUCftZAASb1EVOH/GphHkHwaQu5AU3n8f4+iH3RbnbyBn7EOyNgkUGUD2X0z3O19keB/uqA9kvAcJvItxtoVEjPNmDMVRLjXgoHsT4WgO8vmHaKdUaFAqgdyIZOq5q6HabPCwfuZrzPU20tkDHklRTvIgJwLxYli0AwDQPva9BmGXugAAAABJRU5ErkJggg==');" title="Горячая тема"></div>
 												<?php
 													}
 												?>
